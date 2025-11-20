453ecf25693bea5d7804de8de6df1709f99909c0
Takamatsu Makoto <tkmtmkt@gmail.com>
2025-11-20 22:30:36 +0900

    mod: Specifying port numbers etc. in environment variables

--- docker/tomcat/var/opt/tomcat/example/bin/setenv.sh
+++ docker/tomcat/var/opt/tomcat/example/bin/setenv.sh
@@ -1,13 +1,27 @@
+export HTTP_PORT=8080
+export HTTPS_PORT=8443
+export SHUTDOWN_PORT=8005
+export CONTEXT_PATH=/gitbucket
+export WAR_FILE=${CATALINA_BASE}/gitbucket.war
+
+# 環境変数設定
 export LANG=ja_JP.UTF-8
 export UMASK=0007
-JAVA_VERSION=$(java -version 2>&1|awk '/version/{print $3}'|sed 's/"//g'|cut -d. -f1)
 
-CATALINA_OPTS="-server -Xmx256m -Xms256m -XX:MaxMetaspaceSize=256m -XX:MetaspaceSize=256m -XX:SurvivorRatio=2"
-if [ ${JAVA_VERSION} -ge 9 ]; then
-  CATALINA_OPTS="${CATALINA_OPTS} -Xlog:gc*=info:file="${CATALINA_BASE}/gc_%t.log":time,uptime,level,tags:filecount=10,filesize=10M"
+# JVMオプション
+CATALINA_OPTS="-server -Xmx256m -Xms256M -XX:MaxMetaspaceSize=64M -XX:MetaspaceSize=32M -XX:SurvivorRatio=2"
+JAVA_VERSION=$(java -version 2>&1|awk '/version/{print $3}'|sed 's/"//g'|cut -d. -f1)
+if [ ${JAVA_VERSION} -eq 9 ]; then
+  CATALINA_OPTS="${CATALINA_OPTS} -Xloggc:${CATALINA_BASE}/logs/gc_%t.log"
+  CATALINA_OPTS="${CATALINA_OPTS} -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=10 -XX:GCLogFileSize=10M"
+  CATALINA_OPTS="${CATALINA_OPTS} -XX:+PrintGCDetails -XX:+PrintGCDateStamps"
 else
-  DATE=`date +'%Y%m%d-%H%M%S'`
-  CATALINA_OPTS="${CATALINA_OPTS} -Xloggc:${CATALINA_BASE}/logs/gc.log.${DATE} -XX:+PrintGCDetails -XX:+PrintGCDateStamps"
+  CATALINA_OPTS="${CATALINA_OPTS} -Xlog:gc*=info:file=${CATALINA_BASE}/logs/gc_%t.log:time,uptime,level,tags:filecount=10,filesize=10M"
 fi
 CATALINA_OPTS="${CATALINA_OPTS} -Djava.security.egd=file:/dev/urandom"
+CATALINA_OPTS="${CATALINA_OPTS} -Dtomcat.http.port=${HTTP_PORT}"
+CATALINA_OPTS="${CATALINA_OPTS} -Dtomcat.https.port=${HTTPS_PORT}"
+CATALINA_OPTS="${CATALINA_OPTS} -Dtomcat.shutdown.port=${SHUTDOWN_PORT}"
+CATALINA_OPTS="${CATALINA_OPTS} -Dtomcat.context.path=${CONTEXT_PATH}"
+CATALINA_OPTS="${CATALINA_OPTS} -Dtomcat.war.file=${WAR_FILE}"
 export CATALINA_OPTS
