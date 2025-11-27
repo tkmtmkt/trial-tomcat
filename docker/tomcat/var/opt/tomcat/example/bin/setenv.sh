966a124a61fda6af93a40297cf9e4de8533eb228
Takamatsu Makoto <tkmtmkt@gmail.com>
2025-11-27 22:40:03 +0900

    fix: typo

--- docker/tomcat/var/opt/tomcat/example/bin/setenv.sh
+++ docker/tomcat/var/opt/tomcat/example/bin/setenv.sh
@@ -1,3 +1,4 @@
+# server.xml用の設定
 export HTTP_PORT=8080
 export HTTPS_PORT=8443
 export SHUTDOWN_PORT=8005
@@ -11,10 +12,9 @@ export UMASK=0007
 # JVMオプション
 CATALINA_OPTS="-server -Xmx256m -Xms256M -XX:MaxMetaspaceSize=64M -XX:MetaspaceSize=32M -XX:SurvivorRatio=2"
 JAVA_VERSION=$(java -version 2>&1|awk '/version/{print $3}'|sed 's/"//g'|cut -d. -f1)
-if [ ${JAVA_VERSION} -eq 9 ]; then
-  CATALINA_OPTS="${CATALINA_OPTS} -Xloggc:${CATALINA_BASE}/logs/gc_%t.log"
+if [ ${JAVA_VERSION} -eq 8 ]; then
+  CATALINA_OPTS="${CATALINA_OPTS} -Xloggc:${CATALINA_BASE}/logs/gc_%t.log -XX:+PrintGCDetails -XX:+PrintGCDateStamps"
   CATALINA_OPTS="${CATALINA_OPTS} -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=10 -XX:GCLogFileSize=10M"
-  CATALINA_OPTS="${CATALINA_OPTS} -XX:+PrintGCDetails -XX:+PrintGCDateStamps"
 else
   CATALINA_OPTS="${CATALINA_OPTS} -Xlog:gc*=info:file=${CATALINA_BASE}/logs/gc_%t.log:time,uptime,level,tags:filecount=10,filesize=10M"
 fi
